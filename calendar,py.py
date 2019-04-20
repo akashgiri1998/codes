c71912aeed4851c53ce8ff85da5123c41ce9a469
Akash Giri <40692687+akashgiri1998@users.noreply.github.com>
2019-04-20 22:54:16 +0545

    Add files via upload

---
+++ calendar,py.py
@@ -0,0 +1,5 @@
+import calendar
+
+c=calendar.TextCalendar(calendar.TUESDAY)
+st=c.formatmonth(2018,4,0,0)
+print(st)
---
+++ classes.py
@@ -0,0 +1,17 @@
+class myfun():
+    def fun(self):
+        print("my name is akash")
+
+    def fun2(self,b):
+        print("i live in bharatpur"+b)
+        
+        
+def main():
+    c=myfun()
+    c.fun()
+    c.fun2(" with my parents")
+
+if __name__=="__main__":
+   main()
+         
+ 
