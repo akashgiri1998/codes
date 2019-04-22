3ef4f0a8281ae183c4657ef7a0e6742da78d0e3c
akashgiri1998 <akashgirigiri2@gmail.com>
2019-04-22 23:40:25 +0545

    Code for Insertion Sort

--- InsertionSort.py
+++ InsertionSort.py
@@ -1,2 +1,13 @@
-print("im hungry")
-print("this is insertion sort")
+def InsertionSort(data):
+    n=len(data)
+    for j in range(1,n):
+        key=data[j]
+        i=j-1
+        while i>=0 and data[i]>key:
+            data[i+1]=data[i]
+            i=i-1
+        data[i+1]=key
+
+data=[9,12,234,45,3645,90]        
+InsertionSort(data)
+print(data)
